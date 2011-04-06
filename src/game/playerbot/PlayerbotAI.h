c652f494ca3ff0f77f4df68e79e4123c72aa9040
GMHAZE <iceis2@hotmail.com>
2011-04-05 23:53:44 -0400

    Revert "Merge branch 'master' of git://github.com/blueboy/portal"
    This reverts commit 2490ba829b671af802b641819b115b911e983cbb, reversing
    changes made to a4b7478125ff0b7e07d63a8b8bd23d9e29dcdafa.

--- src/game/playerbot/PlayerbotAI.h
+++ src/game/playerbot/PlayerbotAI.h
@@ -118,8 +118,7 @@ public:
         BOTSTATE_COMBAT,            // bot is in combat
         BOTSTATE_DEAD,              // we are dead and wait for becoming ghost
         BOTSTATE_DEADRELEASED,      // we released as ghost and wait to revive
-        BOTSTATE_LOOTING,           // looting mode, used just after combat
-        BOTSTATE_FLYING             // bot is flying
+        BOTSTATE_LOOTING            // looting mode, used just after combat
     };
 
     enum CollectionFlags
@@ -145,7 +144,6 @@ public:
     typedef std::list<uint64> BotLootCreature;
     typedef std::list<uint32> BotLootEntry;
     typedef std::list<uint32> BotSpellList;
-    typedef std::vector<uint32> BotTaxiNode;
 
     // attacker query used in PlayerbotAI::FindAttacker()
     enum ATTACKERINFOTYPE
@@ -310,8 +308,6 @@ public:
     void SendOrders(Player& player);
     bool FollowCheckTeleport(WorldObject &obj);
     void DoLoot();
-    void DoFlight();
-    void GetTaxi(ObjectGuid guid, BotTaxiNode& nodes);
 
     bool HasCollectFlag(uint8 flag) { return m_collectionFlags & flag; }
     void SetCollectFlag(uint8 flag)
@@ -385,7 +381,6 @@ private:
     ObjectGuid m_lootCurrent;           // current remains of interest
     ObjectGuid m_lootPrev;              // previous loot
     BotLootEntry m_collectObjects;      // object entries searched for in findNearbyGO
-    BotTaxiNode m_taxiNodes;            // flight node chain;
 
     uint8 m_collectionFlags;            // what the bot should look for to loot
 
@@ -398,7 +393,6 @@ private:
     // can do it
     uint32 m_spellIdCommand;
     uint64 m_targetGuidCommand;
-    ObjectGuid m_taxiMaster;
 
     AttackerInfoList m_attackerInfo;
 
