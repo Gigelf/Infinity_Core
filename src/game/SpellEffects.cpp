f06e97cda342c5f38b2cd81349f886b8cf108fc2
GMHAZE <iceis2@hotmail.com>
2011-04-17 13:22:37 -0400

    remove dupe from merge  ( already had it in core )

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -8214,12 +8214,6 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                         m_caster->CastSpell(unitTarget, 52125, false);
                     break;
                 }
-                case 48810:
-                {
-                    if(unitTarget)
-                        unitTarget->CastSpell(unitTarget, 48809, true);
-                    return;
-                }
                 case 48724:                                 // Q: The Denouncement
                 case 48726:
                 case 48728:
