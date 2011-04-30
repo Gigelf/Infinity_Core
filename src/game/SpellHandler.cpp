200eb9eb6d5d6a7413bfd790333fbed575acd8b0
GMHAZE <iceis2@hotmail.com>
2011-04-30 12:42:21 -0400

    Revert "Vehicles: enable some spell-type interaction (TESTING)"
    This reverts commit 4d2f12069f4303001b4dcb0348e4ced3c89012d8.
    
    Conflicts:
    
    	src/game/SpellHandler.cpp

--- src/game/SpellHandler.cpp
+++ src/game/SpellHandler.cpp
@@ -333,8 +333,8 @@ void WorldSession::HandleCastSpellOpcode(WorldPacket& recvPacket)
     recvPacket >> unk_flags;                                // flags (if 0x02 - some additional data are received)
 
     // ignore for remote control state (for player case)
-    Unit* _mover = GetPlayer()->GetMover(); 
-    if (_mover != GetPlayer() && _mover->GetTypeId()==TYPEID_PLAYER) 
+    Unit* mover = _player->GetMover();
+    if (mover != _player && mover->GetTypeId()==TYPEID_PLAYER)
     {
         recvPacket.rpos(recvPacket.wpos());                 // prevent spam at ignore packet
         return;
@@ -355,15 +355,6 @@ void WorldSession::HandleCastSpellOpcode(WorldPacket& recvPacket)
         recvPacket.rpos(recvPacket.wpos());                 // prevent spam at ignore packet
         return;
     }
-
-    //  Players on vehicles may cast many simple spells (like knock) from self 
- 
-    Unit* mover = NULL; 
- 
-    if (spellInfo->AttributesEx6 & SPELL_ATTR_EX6_UNK12 && _mover->IsCharmerOrOwnerPlayerOrPlayerItself()) 
-        mover = _mover->GetCharmerOrOwnerPlayerOrPlayerItself(); 
-    else 
-        mover = _mover;
 
     // players should be able to open chests in Malygos encounter
     if (spellId == 61437 && _player->GetVehicle() && _player->GetVehicle()->GetBase()->GetVehicleInfo()->GetEntry()->m_ID == 30161)
