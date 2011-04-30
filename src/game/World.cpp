e08a6ca5478f1442a16a6bcfb2bfbee710d46c75
GMHAZE <iceis2@hotmail.com>
2011-04-30 04:19:40 -0400

    more minor fixes to pvp token

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -835,8 +835,8 @@ void World::LoadConfigSettings(bool reload)
     setConfig(CONFIG_FLOAT_PVP_TOKEN_ITEMID,"PvPToken.ItemID", 29434);
     setConfig(CONFIG_FLOAT_PVP_TOKEN_ITEMCOUNT,"PvPToken.ItemCount", 1);
     setConfig(CONFIG_FLOAT_PVP_TOKEN_GOLD,"PvPToken.Gold", 100000);
-    setConfig(CONFIG_PVP_TOKEN_HONOR,"PvPToken.Honor", 75000);
-    setConfig(CONFIG_PVP_TOKEN_ARENA,"PvPToken.Arena", 5000);
+    setConfig(CONFIG_FLOAT_PVP_TOKEN_HONOR,"PvPToken.Honor", 75000);
+    setConfig(CONFIG_FLOAT_TOKEN_ARENA,"PvPToken.Arena", 5000);
     setConfig(CONFIG_FLOAT_PVP_TOKEN_RESTRICTION,"PvPToken.MapRestriction", 4);
 
     if(getConfig(CONFIG_FLOAT_PVP_TOKEN_ITEMCOUNT) < 1)
