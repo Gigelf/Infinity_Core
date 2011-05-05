45877afceacffaaf0d909affcd4c69392b9119f5
GMHAZE <iceis2@hotmail.com>
2011-05-05 13:02:46 -0400

    crashfix by ankso

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -10913,7 +10913,7 @@ void Unit::ProcDamageAndSpellFor( bool isVictim, Unit * pTarget, uint32 procFlag
 
     // Frost Nova
     if (itr->second->GetCaster() == pTarget)
-      if (SpellEntry const* spellInfo = itr->second->GetSpellProto())
+      if (SpellEntry const* spellInfo = itr->second->GetSpellProto() && procSpell)
         if (spellInfo->SpellFamilyName == SPELLFAMILY_MAGE && spellInfo->SpellFamilyFlags & UI64LIT(0x000000000000000000000040)
           && procSpell->SpellFamilyName == SPELLFAMILY_MAGE && procSpell->SpellFamilyFlags & UI64LIT(0x000000000000000000000040))
           continue;
